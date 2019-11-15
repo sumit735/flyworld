74672b6341590fd39f8fa1d6e28820a6cedd2fc8
sumit735 <sumitkumardash1999@gmail.com>
2019-11-15 18:35:29 +0530

    added header and footer

---
+++ includes/header.php
@@ -0,0 +1,232 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Flyworld</title>
+    <link rel="icon" type="image/png" href="assets/images/favicon.png">
+    <!-- ==================Start Css Link===================== -->
+<link href="https://fonts.googleapis.com/css?family=Lato&amp;display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
+<link rel="stylesheet" href="assets/css/animate.css">
+<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+<link rel="stylesheet" href="assets/css/datepicker.min.css">
+<link rel="stylesheet" href="assets/css/OverlayScrollbars.min.css">
+<link rel="stylesheet" href="assets/css/fontawesome.css">
+<link rel="stylesheet" href="assets/css/icofont.css">
+<link rel="stylesheet" href="assets/css/magnific-popup.css">
+<link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+<link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+<link rel="stylesheet" href="assets/css/slick.css">
+<link rel="stylesheet" href="assets/css/slick-theme.css">
+<link rel="stylesheet" href="assets/css/slider-range.css">
+<link rel="stylesheet" href="assets/css/select2.min.css">
+<link rel="stylesheet" href="assets/css/tippy.css">
+<link rel="stylesheet" href="assets/css/app.css">
+<!-- ==================End Css Link===================== -->
+
+<style>
+
+    .col {
+        background: #ff00cc;  /* fallback for old browsers */
+        background: -webkit-linear-gradient(to right, #333399, #ff00cc);  /* Chrome 10-25, Safari 5.1-6 */
+        background: linear-gradient(to right, #333399, #ff00cc); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+    }
+
+</style>
+
+
+    <!--[if lt IE 9]>
+<script src="http://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="http://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+</head>
+
+
+<body>
+
+
+  <!-- 
+        ================== Header Area Start===================
+     -->
+    
+  <div class="rt-preloder">
+    <div class="preloder-box">
+        <img src="assets/images/all-img/preloder.svg" alt="preloder image" draggable="false">
+    </div><!-- /.preloder-box -->
+</div><!-- /.rt-preloder -->
+
+  <header class="rt-site-header  rt-fixed-top white-menu">
+    <div class="top-header d-none d-md-block">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-md-6">
+                <ul class="text-center text-md-left top-message">
+                    <!-- <li> <i class="icofont-headphone-alt"></i>Support</li> -->
+                    <li><a href="mailto:info@flyworld.com " style="color: #fff;"><i class="icofont-email" style="color: #fff;"></i>info@flyworld.com</a></li>
+                </ul>
+            </div><!-- end top header single -->
+            <div class="col-md-6">
+                <ul class="text-center text-md-right top-social">
+                    <!-- <li><a href="#" class="dropdown-toggle" id="dropdownMenuButton" data-toggle="dropdown"
+        aria-haspopup="true" aria-expanded="false"><i class="fas fa-globe-americas"></i>Englishs</a>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <a class="dropdown-item" href="#">English</a>
+                            <a class="dropdown-item" href="#">French</a>
+                            <a class="dropdown-item" href="#">Trukis</a>
+                        </div>
+                        </li>
+                    <li><a href="#"><i class="fas fa-briefcase"></i>My Trips</a></li> -->
+                    <li><a href="#" data-target="#rtmodal-1" data-toggle="modal"><i class="far fa-user-circle"></i>Sign in | Join</a></li>
+                </ul>
+            </div><!-- end top header single -->
+        </div>
+    </div>
+</div> <!-- end top header -->
+
+
+    <div class="main-header rt-sticky">
+      <nav class="navbar">
+        <div class="container">
+            <!-- <a href="index-2.php" class="brand-logo"><img src="assets/images/logo/logo.png" alt=""></a> -->
+            <!-- <a href="index-2.php" class="sticky-logo"><img src="assets/images/logo/logo.png" alt=""></a> -->
+          <div class="ml-auto d-flex align-items-center">
+            
+
+<div class="main-menu">
+    <ul>
+        <li class="current-menu-item"><a href="#home" style="color: black;">Home</a></li>
+        <li class="menu-item-has-children"><a href="#" style="color: black;">Holidays</a>
+            <ul class="sub-menu">
+                <li class="sub-menu"><a href="#">Odisha Packages</a></li>
+                <li class="sub-menu"><a href="#">India Packages</a></li>
+                <li class="sub-menu"><a href="#">International Packages</a></li>
+            </ul>
+        </li>
+        
+        <li class="menu-item-has-children"><a href="#" style="color: black;">Car Rentals</a>
+            <ul class="sub-menu">
+                <li class="sub-menu"><a href="#">Indigo</a></li>
+                <li class="sub-menu"><a href="#">Swift</a></li>
+                <li class="sub-menu"><a href="#">Swift Dzire</a></li>
+                <li class="sub-menu"><a href="#">Tiago</a></li>
+                <li class="sub-menu"><a href="#">Innova</a></li>
+                <li class="sub-menu"><a href="#">Bolero</a></li>
+                <li class="sub-menu"><a href="#">Tavera</a></li>
+            </ul>
+        </li>
+        <li><a href="service-1.php" style="color: black;">Luxury Cars</a></li>
+        <li> <a href="blog.php" style="color: black;">Wedding Cars</a></li>
+        <li class="menu-item-has-children"><a href="#" style="color: black;">Places</a>
+            <ul class="sub-menu">
+                <li class="sub-menu"><a href="#">Bhubaneswar</a></li>
+                <li class="sub-menu"><a href="#">Cuttack</a></li>
+                <li class="sub-menu"><a href="#">Puri</a></li>
+                <li class="sub-menu"><a href="#">Jajpur</a></li>
+                <li class="sub-menu"><a href="#">Berhampur</a></li>
+            </ul>
+        </li>
+      
+        
+    
+    </ul>
+    <div class="mobile-menu">
+        <div class="menu-click">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </div>
+</div><!-- end main menu -->
+            <div class="rt-nav-tolls d-flex align-items-center">
+    <span class="d-md-inline d-none"><a href="contact.php" class="rt-btn rt-gradient2 rt-rounded text-uppercase rt-Bshadow-1">Contact
+            Us</a></span>
+</div>
+    
+          </div>
+        </div>
+      </nav>
+    </div><!-- /.bootom-header -->
+
+  </header>
+
+  <!-- Modal -->
+<div class="modal fade" id="rtmodal-1" tabindex="-1" role="dialog" aria-labelledby="rtmodal-1"
+    aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered rt-lgoinmodal " role="document">
+        <div class="modal-content">
+            <div class="modal-body">
+                <div class="rt-modal-headr rt-mb-20 one">
+                    <img src="assets/images/logo/Logo-icon.png" alt="modal logo" draggable="false">
+                    <h4>Login in to flyworld</h4>
+                    <p>Log in to get in the moment updates on the things
+                        that interest you.</p>
+                </div><!-- /.rt-modal-headr -->
+                <div class="rt-modal-headr rt-mb-20 two">
+                    <img src="assets/images/logo/Logo-icon.png" alt="modal logo" draggable="false">
+                    <h4>Create your Account</h4>
+                    <p>Log in to get in the moment updates on the things
+                        that interest you.</p>
+                </div><!-- /.rt-modal-headr -->
+                <div class="rt-modal-input one">
+                    <form action="#" class="rt-form">
+                        <input type="text" class="form-control pill rt-mb-15" placeholder="User name">
+                        <input type="password" class="form-control pill rt-mb-15" placeholder="Password">
+                        <div class="form-group forgot-pass">
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" id="gridCheckss">
+                                <label class="form-check-label" for="gridCheckss">
+                                     Remember Password
+                                </label>
+                            </div><!-- ./orm-group -->
+                        </div>
+                            <input type="submit" class="rt-btn rt-gradient pill d-block text-uppercase " value="Log In">
+                    </form>
+                    <div class="ac-register">
+                        <span>Don’t have an account? <a href="#" class="open-creatac">Sign Up Now <i class="icofont-double-right"></i></a></span>
+                    </div><!-- /.ac-register -->
+                </div><!-- /.rt-modal-input -->
+                <div class="rt-modal-input two">
+                    <form action="#" class="rt-form">
+                        <input type="text" class="form-control pill rt-mb-15" placeholder="User name">
+                        <input type="password" class="form-control pill rt-mb-15" placeholder="Enter your mail address">
+                        <input type="password" class="form-control pill rt-mb-15" placeholder="Password">
+                        <div class="form-group forgot-pass">
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" id="gridCheck333">
+                                <label class="form-check-label" for="gridCheck333">
+                                    I'd like to hear about promos, new products,and much more!
+                                </label>
+                            </div><!-- ./orm-group -->
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" id="gridCheck222">
+                                <label class="form-check-label" for="gridCheck222">
+                                    By clicking "Sign up" you agree to our Terms of Service and Privacy Policy
+                                </label>
+                            </div><!-- ./orm-group -->
+                        </div>
+                            <input type="submit" class="rt-btn rt-gradient pill d-block text-uppercase " value="Log In">
+                    </form>
+                    <div class="ac-register">
+                        <span>Already have an account?<a href="#">LOGIN <i class="icofont-double-right"></i></a></span>
+
+                    </div><!-- /.ac-register -->
+                </div><!-- /.rt-modal-input -->
+                <div class="rt-modal-footer">
+                    <span>Or</span>
+                    <h4>Sign Up with social media</h4>
+                    <ul class="rt-social rt-circle-style2">
+                
+                        <li><a href="#"><i class="icofont-facebook"></i></a></li>
+                        <li><a href="#"><i class="icofont-twitter"></i></a></li>
+                        <li><a href="#"><i class="icofont-linkedin"></i></a></li>
+                    </ul>
+                </div><!-- /.rt-modal-footer -->
+            </div>
+        </div>
+    </div>
+</div>
---
+++ test.php
@@ -0,0 +1,72 @@
+<?php include "includes/header.php"; ?>
+
+
+<section class="rt-banner-area">
+    <div class="single-rt-banner rt-banner-height">
+        <div class="container">
+            <div class="row  rt-banner-height align-items-center">
+                <h1>Hello</h1>
+            </div><!-- end row -->
+        </div><!-- end container -->
+    </div><!-- end single rt banner -->
+</section>
+
+<!-- 
+    ===========Services Area start==========
+ -->
+<section class="emigr-services-area rtbgprefix-contain" style="background-image: url(assets/images/backgrounds/dotbg.png)">
+    <h1>satya</h1>
+</section>
+
+
+<!-- 
+    ============Deal Area Start===============
+ -->
+
+
+ <section class="deal-area">
+    <h1>silu</h1>
+ </section>
+ <!-- ==================================================================================== -->
+<!-- India Packages -->
+<!-- ===================================================================================== -->
+
+ <section class="deal-area">
+   <h1>Sumit</h1>
+ </section>
+<!-- 
+    ============ Portfolio Start===============
+ -->
+<div class="spacer-top"></div><!-- /.spacer-top -->
+<section class="portfolio-area rt-section-padding rtbgprefix-full bg-hide-md gradinet-bg-md">
+    <h1>World</h1>
+</section>
+
+<!-- 
+    ======== Flight deals start======
+ -->
+
+<div class="spacer-top"></div><!-- /.spacer-top -->
+
+<section class="flight-dela-area" data-scrollax-parent="true">
+    <h1>Hello</h1>
+</section>
+
+
+<!-- 
+    ============= Testimonilas area start==========
+ -->
+<section class="testimonials-area" data-scrollax-parent="true">
+    <h1>Hello World</h1>
+</section>
+
+<?php include "includes/footer.php"; ?>
+
+
+
+
+
+
+
+
+
