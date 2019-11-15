09673c56ac32c522d31871a2812395c2f0c2b6e4
Satya-N <satyaprakashn7@gmail.com>
2019-11-15 12:54:10 +0530

    holidays

--- index.php
+++ index.php
@@ -91,7 +91,24 @@
 <div class="main-menu">
     <ul>
         <li class="current-menu-item"><a href="#home" style="color: black;">Home</a></li>
-        <li><a href="trip-1.html">Holidays</a></li>
+        <li class="menu-item-has-children"><a href="#">Holidays</a>
+            <ul class="sub-menu">
+                <li class="menu-item-has-children"><a href="#">Home Pages</a>
+                    <ul class="sub-menu">
+                        <li><a href="index-2.html">Home One</a></li>
+                        <li><a href="index-02.html">Home Two</a></li>
+                    </ul>
+                </li>
+                <li class="menu-item-has-children"><a href="#">Hotel Pages</a>
+                    <ul class="sub-menu">
+                        <li><a href="hotel-1.html">Hotel One</a></li>
+                        <li><a href="hotel-2.html">Hotel Two</a></li>
+                        <li><a href="hotel-3.html">Hotel Three</a></li>
+                    </ul>
+                </li>
+                
+            </ul>
+        </li>
         
         <li class="menu-item-has-children"><a href="#">Car Rentals</a>
             <ul class="sub-menu">
@@ -217,6 +234,8 @@
                 </li>
             </ul>
         </li>
+      
+        
     
     </ul>
     <div class="mobile-menu">
