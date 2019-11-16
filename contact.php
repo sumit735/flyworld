45b923edd2bb4300d6aad5b802880965c4277738
Satya-N <satyaprakashn7@gmail.com>
2019-11-16 11:19:23 +0530

    contact page full

--- contact.php
+++ contact.php
@@ -198,7 +198,7 @@
     ============= Brands area start==========
  -->
 
-<section class="brands-area">
+ <!-- <section class="brands-area">
     <div class="container">
         <div class="row">
             <div class="col-lg-9 mx-auto text-center">
@@ -206,16 +206,16 @@
                     <h2 class="rt-section-title">
                         <span>Take a Look at Our</span>
                         Trusted Partners
-                    </h2><!-- /.rt-section-title -->
+                    </h2>
                     <p>
                         We are committed to being the best partner.Emigrar believes in being your trusted partner and
                         earning that trust through
                         confidence and performance in service and support.
                     </p>
-                </div><!-- /.rt-section-title-wrapper -->
-            </div><!-- /.col-lg-9 -->
-        </div><!-- /.row -->
-        <div class="section-title-spacer"></div><!-- /.section-title-spacer -->
+                </div>
+            </div>
+        </div>
+        <div class="section-title-spacer"></div>
         <div class="row">
             <div class="col-lg-9 mx-auto">
                 <ul class="rt-border-brands">
@@ -223,47 +223,47 @@
         <a href="#" class="wow flipInX d-block">
             <img src="assets/images/brands/brands-1.png" alt="brands image" draggable="false">
         </a>
-    </li><!-- /.single-border-brands -->
+    </li>
     <li class="single-border-brands">
         <a href="#" class="wow flipInX d-block" data-wow-duration="1s">
             <img src="assets/images/brands/brands-2.png" alt="brands image" draggable="false">
         </a>
-    </li><!-- /.single-border-brands -->
+    </li>
     <li class="single-border-brands">
         <a href="#" class="wow flipInX d-block" data-wow-duration="1.5s">
             <img src="assets/images/brands/brands-3.png" alt="brands image" draggable="false">
         </a>
-    </li><!-- /.single-border-brands -->
+    </li>
     <li class="single-border-brands">
         <a href="#" class="wow flipInX d-block" data-wow-duration="2s"> 
             <img src="assets/images/brands/brands-4.png" alt="brands image" draggable="false">
         </a>
-    </li><!-- /.single-border-brands -->
+    </li>
     <li class="single-border-brands">
         <a href="#" class="wow flipInX d-block" data-wow-duration="2.5s">
             <img src="assets/images/brands/brands-4.png" alt="brands image" draggable="false">
         </a>
-    </li><!-- /.single-border-brands -->
+    </li>
     <li class="single-border-brands">
         <a href="#" class="wow flipInX d-block" data-wow-duration="3s">
             <img src="assets/images/brands/brands-3.png" alt="brands image" draggable="false">
         </a>
-    </li><!-- /.single-border-brands -->
+    </li>
     <li class="single-border-brands">
         <a href="#" class="wow flipInX d-block" data-wow-duration="3.5s">
             <img src="assets/images/brands/brands-2.png" alt="brands image" draggable="false">
         </a>
-    </li><!-- /.single-border-brands -->
+    </li>
     <li class="single-border-brands">
         <a href="#" class="wow flipInX d-block" data-wow-duration="4s">
             <img src="assets/images/brands/brands-1.png" alt="brands image" draggable="false">
         </a>
-    </li><!-- /.single-border-brands -->
-</ul><!-- /.rt-border-brands -->
-            </div><!-- /.col-lg-7 -->
-        </div><!-- /.row -->
-    </div><!-- /.containe -->
-</section>
+    </li>
+</ul>
+            </div>
+        </div>
+    </div>
+</section> -->
 
 <!-- 
     !============= Footer Area Start ===========!
