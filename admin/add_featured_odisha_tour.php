a1487bbd6e03420365d1d084e5dc9949fbf9c3bd
mkpanda321 <mkpanda111@gmail.com>
2019-11-18 16:36:16 +0530

    header added

--- admin/add_featured_odisha_tour.php
+++ admin/add_featured_odisha_tour.php
@@ -1,12 +1,29 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Add Odisha Tour</title>
-</head>
-<body>
-	
-</body>
-</html>
+<?php include "../header.php"; ?>	
+
+<form>
+  <div class="form-group col-md-6">
+    <label for="Price">Tour Name :</label>
+    <input type="text" class="form-control" id="inputAddress2">
+  </div>
+    <div class="form-group col-md-6">
+    <label for="Price">Tour Details :</label>
+    <input type="text" class="form-control" id="inputAddress2">
+  </div>
+  <div class="form-group col-md-6">
+    <label for="inputAddress">Image</label>
+    <input type="file" class="form-control-file" id="exampleFormControlFile1">
+  </div>
+  <div class="form-group col-md-6">
+    <label for="Price">Price :</label>
+    <input type="text" class="form-control" id="inputAddress2">
+  </div>
+  <div class="form-group col-md-6">
+    <label for="Price">Cities Covered:</label>
+    <input type="text" class="form-control" id="inputAddress2">
+  </div>
+  <div class="form-group col-md-6">
+  <button type="submit" class="btn btn-primary">Submit</button>
+</div>
+</form>
+
+<?php include "footer.php"; ?>
---
+++ admin/footer.php
@@ -0,0 +1,107 @@
+<!-- 
+    !============= Footer Area Start ===========!
+ -->
+ <section class="rt-site-footer" data-scrollax-parent="true">
+   <div class="footer-top footstyle">
+        <div class="section-title-spacer"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-9">
+                    <div class="rt-single-widget wow fade-in-bottom" data-wow-duration="1s">
+                        <h3 class="rt-footer-title">Company Info</h3>
+                        <ul class="rt-usefulllinks">
+                            <li>
+                                <a href="#">About Us</a>
+                            </li>
+                            <li>
+                                <a href="#">Contact Us</a>
+                            </li>
+                            <li>
+                                <a href="#">Authenticity Guarantee</a>
+                            </li>
+                            <li>
+                                <a href="#">Customer Reviews</a>
+                            </li>
+                            <li>
+                                <a href="#">Privacy Policy</a>
+                            </li>
+                            <li>
+                                <a href="#">Business License</a>
+                            </li>
+                            
+                        </ul>
+
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="rt-single-widget wow fade-in-bottom" data-wow-duration="1.5s">
+                        <h3 class="rt-footer-title">Address</h3>
+                        <ul class="rt-usefulllinks">
+                            <li><a href="#">Become Partner</a></li>
+                            <li><a href="#">Careers</a></li>
+                            <li><a href="#">Become Affiliate</a></li>
+                            <li><a href="#">Associations</a></li>
+                            <li><a href="#">Advertise with us</a></li>
+                            <li><a href="#">Retirement Plan</a></li>
+                            <li><a href="#">Travel APIs</a></li>
+                        </ul>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    <!-- </div> -->
+    
+</section>
+<div class="footer footstyle">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 text-center text-lg-left">
+                <div class="copy-text wow fade-in-bottom" data-wow-duration="1s">
+                    <p style="color: #fff;">Copyright © 2018.All Rights Reserved.</p>
+                </div><!-- /.copy-text -->
+            </div><!-- /.col-lg-6 -->
+            <div class="col-lg-6 text-center text-lg-right">
+                <div class="copy-text wow fade-in-bottom" data-wow-duration="1s">
+                    <p style="color: #fff;">Designed And Developed By <a href="https://www.gvitechnology.com" target="_blank">GVI Technology.</p>
+                </div>
+            </div><!-- /.col-lg-6 -->
+        </div><!-- /.row -->
+    </div>
+</div>
+
+
+<!-- ==================Start Js Link===================== -->
+<script src="assets/js/jquery-2.2.4.min.js"></script>
+<script src="assets/js/popper.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/moment.min.js"></script>
+<script src="assets/js/jquery.easing.1.3.js"></script>
+<script src="assets/js/imagesloaded.pkgd.min.js"></script>
+<script src="assets/js/isotope.pkgd.min.js"></script>
+<script src="assets/js/instafeed.min.js"></script>
+<script src="assets/js/waypoints.min.js"></script>
+<script src="assets/js/jquery.counterup.min.js"></script>
+<script src="assets/js/jquery.magnific-popup.min.js"></script>
+<script src="assets/js/jquery.scrollUp.min.js"></script>
+<script src="assets/js/owl.carousel.min.js"></script>
+<script src="assets/js/TweenMax.min.js"></script>
+<script src="assets/js/scrollax.min.js"></script>
+<script src="https://maps.googleapis.com/maps/api/js?v=3&amp;key=AIzaSyCy7becgYuLwns3uumNm6WdBYkBpLfy44k"></script>
+<script src="assets/js/wow.js"></script>
+<script src="assets/js/jquery.overlayScrollbars.min.js"></script>
+<script src="assets/js/jquery-ui.js"></script>
+<script src="assets/js/jquery.appear.js"></script>
+<script src="assets/js/select2.min.js"></script>
+<script src="assets/js/slick.min.js"></script>
+<script src="assets/js/slider-range.js"></script>
+<script src="assets/js/vivus.min.js"></script>
+<script src="assets/js/tippy.all.min.js"></script>
+<script src="assets/js/app.js"></script>
+
+<!-- ==================End Js Link===================== -->
+
+</body>
+
+
+</html>
